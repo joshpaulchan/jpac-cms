09fde825d87f73a0856938e9fe9e8885a136e4b0
joshpaulchan <joshpaulchan@gmail.com>
2018-12-25 23:00:12 -0500

    Fix conditionals

--- docker_push.sh
+++ docker_push.sh
@@ -3,13 +3,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 REPOSITORY_NAME="joshpaulchan/cms"
 TAG="latest"
-if [ $TRAVIS_PULL_REQUEST ] then
-    $TAG=$TRAVIS_PULL_REQUEST_BRANCH
+if [ $TRAVIS_PULL_REQUEST ]; then
+    TAG=$TRAVIS_PULL_REQUEST_BRANCH
 else
-    $TAG=$TRAVIS_BRANCH
+    TAG=$TRAVIS_BRANCH
 fi
 # expansion maps '/' in branch names to '-'
-ESCAPED_TAG=$($TAG | tr '/' '-')
+ESCAPED_TAG=$(tr '/' '-' <<< "$TAG")
 echo "Tagging Docker build as: $REPOSITORY_NAME:$ESCAPED_TAG. "
 docker build --rm -t $REPOSITORY_NAME:$ESCAPED_TAG .
 docker push $REPOSITORY_NAME
